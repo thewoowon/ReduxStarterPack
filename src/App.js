f6322fd7c560769a39634bbc664f5df2347b28df
thewoowon <thewoowon@naver.com>
2022-06-17 22:35:00 +0900

    20220617_ReduxPromise

--- src/App.js
+++ src/App.js
@@ -2,26 +2,43 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as counterActions from './modules/counter';
-import axios from 'axios';
+import * as postActions from './modules/post';
 
 
 class App extends Component {
 
+    componentDidMount() {
+        // 컴포넌트가 처음 마운트 될 때 현재 number 를 postId 로 사용하여 포스트 내용을 불러옵니다.
+        const { number, PostActions } = this.props;
+        PostActions.getPost(number);
+    }
 
-componentDidMount(){
-    axios.get('https://jsonplaceholder.typicode.com/posts/1')
-    .then(response => console.log(response.data));
-}
+    componentWillReceiveProps(nextProps) {
+        const { PostActions } = this.props;
+
+        // 현재 number 와 새로 받을 number 가 다를 경우에 요청을 시도합니다.
+        if(this.props.number !== nextProps.number) {
+            PostActions.getPost(nextProps.number)
+        }
+    }
 
     render() {
-        const { CounterActions, number } = this.props;
+        const { CounterActions, number, post, error, loading } = this.props;
 
-        
         return (
             <div>
-                <h1>{number}</h1>
-                <button onClick={CounterActions.incrementAsync}>+</button>
-                <button onClick={CounterActions.decrementAsync}>-</button>
+                <p>{number}</p>
+                <button onClick={CounterActions.increment}>+</button>
+                <button onClick={CounterActions.decrement}>-</button>
+                { loading && <h2>로딩중...</h2>}
+                { error 
+                    ? <h1>에러발생!</h1> 
+                    : (
+                        <div>
+                            <h1>{post.title}</h1>
+                            <p>{post.title}</p>
+                        </div>
+                    )}
             </div>
         );
     }
@@ -29,9 +46,13 @@ componentDidMount(){
 
 export default connect(
     (state) => ({
-        number: state.counter
+        number: state.counter,
+        post: state.post.data,
+        loading: state.post.pending,
+        error: state.post.error
     }),
     (dispatch) => ({
-        CounterActions: bindActionCreators(counterActions, dispatch)
+        CounterActions: bindActionCreators(counterActions, dispatch),
+        PostActions: bindActionCreators(postActions, dispatch)
     })
 )(App);
--- src/modules/post.js
+++ src/modules/post.js
@@ -10,3 +10,60 @@ const GET_POST_PENDING = 'GET_POST_PENDING';
 const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
 const GET_POST_FAILURE = 'GET_POST_FAILURE';
 
+export const getPost = (postId) => dispatch =>{
+    dispatch({type : GET_POST_PENDING});
+
+    return getPostAPI(postId).then(
+        (response) =>{
+            dispatch({
+                type:GET_POST_SUCCESS,
+                payload:response
+            })
+        }
+    ).catch(error=>{
+        dispatch({
+            type:GET_POST_FAILURE,
+            payload:error
+        });
+        throw(error);
+    })
+}
+
+
+const initialState = {
+    pending :false,
+    error:false,
+    data:{
+        title:'',
+        body:''
+    }
+}
+
+
+export default handleActions({
+    [GET_POST_PENDING]:(state,action) =>{
+        return {
+            ...state,
+            pending:true,
+            error:false
+        }
+    },
+    [GET_POST_SUCCESS]:(state,action)=>{
+        const {title, body} = action.payload.data;
+
+        return {
+            ...state,
+            pending:false,
+            data:{
+                title,body
+            }
+        }
+    },
+    [GET_POST_FAILURE]:(state,action) =>{
+        return {
+            ...state,
+            pending:false,
+            error:true
+        }
+    }
+},initialState);
